8df1597fb59f654f9855d51a41a36dc337aa9a82
Staš Kotar Celarc <stas.kc@gmail.com>
2017-01-23 01:04:30 +0100

    popravil napake v csv-jih

--- requestPageSource.py
+++ requestPageSource.py
@@ -8,7 +8,7 @@ def requestTextFromWebsite(naslov):
     except requests.exceptions.ConnectionError:
         print('connection error')
     else:
-        return r.text;
+        return r.text
 
 def requestCragHtml(id):
     with open('html/miska.html', 'w') as out:
@@ -24,39 +24,56 @@ def requestDataHtml(location):
             details.write(requestTextFromWebsite('http://www.plezanje.net/climbing/db/showRoute.asp?route=' + str(route_id.group('id'))))
             #print(route_id.group('id'))
 
-def cleanSmeri(dict):
+def cleanSmeri(route):
+    ocena1_pattern = re.compile(r'<dt>Težavnost</dt>.*?<dd>(?P<ocena>.+?)</dd>', flags=re.DOTALL)
+    dolzina_pattern = re.compile(r'<dt>Dolžina smeri \(m\)</dt>.*?<dd id="RouteHeigth">(?P<dolzina>\d+)</dd>', flags=re.DOTALL)
+    vzponi_pattern = re.compile(r'<dt>Zabeleženih vzponov</dt>.*?<dd>(?P<vzponi>.+?)</dd>', flags=re.DOTALL)
+    ocena = re.search(ocena1_pattern, route.group('tabela1'))
+    dolzina = re.search(dolzina_pattern, route.group('tabela1'))
+    vzponi = re.search(vzponi_pattern, route.group('tabela1'))
     out = {}
-    out['id'] = int(dict['id'])
-    out['ime'] = dict['ime'].strip()
-    out['ocena'] = dict['ocena'].strip()
-    out['dolzina'] = int(dict['dolzina'])
-    out['vzponi'] = dict['vzponi'].strip()
+    route_dict = route.groupdict()
+    out['id'] = int(route_dict['id'])
+    out['ime'] = route_dict['ime'].strip()
+    if ocena: out['ocena'] = ocena.groupdict()['ocena'].strip()
+    else: out['ocena'] = ''
+    if dolzina: out['dolzina'] = int(dolzina.groupdict()['dolzina'])
+    else: out['dolzina'] = 0
+    if vzponi: out['vzponi'] = vzponi.groupdict()['vzponi'].strip()
+    else: out['vzponi'] = ''
     return out
 
-def cleanOcene(ocena, smer, id):
+def cleanOcene(tabela, smer, id):
+    info_pattern = re.compile(r'<td valign="top">(?P<info>.+?)</td>', flags=re.DOTALL)
+    plezalec_pattern = re.compile(r'<td valign="top">(?P<plezalec>.+?)</td>', flags=re.DOTALL)
+    datum_pattern = re.compile(r'<td valign="top">(?P<datum>.+?)</td>', flags=re.DOTALL)
     out = {}
+    smer_dict = smer.groupdict()
+    roles = ['ocena', 'plezalec', 'datum']
+    i = 0
     out['id'] = id
-    out['smer_id'] = int(smer['id'])
-    out['ime'] = smer['ime'].strip()
-    out['ocena'] = ocena['ocena'].strip()
-    out['plezalec'] = ocena['plezalec'].strip()
-    out['datum'] = ocena['datum'].strip()
-    out['valid'] = ocena['valid'].strip()
+    out['smer_id'] = int(smer_dict['id'])
+    out['ime'] = smer_dict['ime'].strip()
+    out['ocena'] = ''
+    out['plezalec'] = ''
+    out['datum'] = ''
+    for info in re.finditer(info_pattern, tabela.group('tabela3')):
+        if info:
+            out[roles[i]] = info.groupdict()['info'].strip()
+        else:
+            out[roles[i]] = ''
+        i += 1
+    out['valid'] = tabela.groupdict()['valid'].strip()
     return out
 
 def extractRouteDetails(location):
 
     route_pattern = re.compile(r'<!DOCTYPE html PUBLIC.*?xp:id="(?P<id>\d+)".*?'
                                r'<title>Smer v plezališču: (?P<ime>.+?)</title>.*?'
-                               r'<dt>Težavnost</dt>.*?<dd>(?P<ocena>.+?)</dd>.*?'
-                               r'<dt>Dolžina smeri \(m\)</dt>.*?<dd id="RouteHeigth">(?P<dolzina>\d+)</dd>.*?'
-                               r'<dt>Zabeleženih vzponov</dt>.*?<dd>(?P<vzponi>.*?)</dd>.*?'
-                               r'<div class="formSection">.*?<p class="formSectionTitle">Ocene</p>(?P<tabela>.*?)</div>', flags=re.DOTALL)
+                               r'<dl class="dlTable">(?P<tabela1>.+?)</dl>.*?'
+                               r'<div style="clear: both;">(?P<tabela2>.*?)<div style="clear: both;">', flags=re.DOTALL)
 
-    ocena_pattern = re.compile(r'<tr class="(?P<valid>.*?)">.*?'
-                               r'<td valign="top">(?P<ocena>.+?)</td>.*?'
-                               r'<td valign="top">(?P<plezalec>.*?)</td>.*?'
-                               r'<td valign="top">(?P<datum>.*?)</td>', flags=re.DOTALL)
+    tabela2_pattern = re.compile(r'<tr class="(?P<valid>.*?)">(?P<tabela3>.*?)</tr>', flags=re.DOTALL)
 
     smeri = []
     ocene = []
@@ -64,23 +81,22 @@ def extractRouteDetails(location):
     with open('csv/smeri.csv', 'w') as route_csv, open('csv/ocene.csv', 'w') as ocene_csv, open(location, 'r') as routes_file:
         routes_re = routes_file.read()
         for route_re in re.finditer(route_pattern, routes_re):
-            clean_route = route_re.groupdict()
-            smeri.append(cleanSmeri(clean_route))
             print(route_re.group('ime'))
-            for ocena_re in re.finditer(ocena_pattern, route_re.group('tabela')):
-                ocene.append(cleanOcene(ocena_re.groupdict(), clean_route, ocene_id))
-                print(ocena_re.group('ocena'))
+            smeri.append(cleanSmeri(route_re))
+            for tabela_re in re.finditer(tabela2_pattern, route_re.group('tabela2')):
+                print(tabela_re.group('tabela3'))
+                ocene.append(cleanOcene(tabela_re, route_re, ocene_id))
                 ocene_id += 1
 
         smeri_writer = csv.DictWriter(route_csv, fieldnames = ['id', 'ime', 'ocena', 'dolzina', 'vzponi'])
         smeri_writer.writeheader()
-        for route in smeri:
-            smeri_writer.writerow(route)
+        for route_out in smeri:
+            smeri_writer.writerow(route_out)
 
         ocene_writer = csv.DictWriter(ocene_csv, fieldnames=['id', 'smer_id', 'ime', 'ocena', 'plezalec', 'datum', 'valid'])
         ocene_writer.writeheader()
-        for ocena in ocene:
-            ocene_writer.writerow(ocena)
+        for ocena_out in ocene:
+            ocene_writer.writerow(ocena_out)
 
 
 
